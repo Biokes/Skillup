dc1131ef99f615152c68a14a08af2830e6f2d35b
Biokes <106444765+Biokes@users.noreply.github.com>
2025-10-27 00:01:26 +0100

    update: add background image to hub

--- frontend/src/pages/GameHub.tsx
+++ frontend/src/pages/GameHub.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export default function GameHub() {
     const navigate = useNavigate()
     return (
-        <main className="gap-4">
+        <main className="gap-4 gameHub">
             <nav className="fixed h-[80px] flex justify-between py-1 px-3 bg-muted/20 w-full z-10 glass ">
                 <aside className="w-[100px] h-[50px] cursor-pointer" onClick={() =>navigate('/')}>
                     <img src="/logo.png" alt="" className="object-cover object-center" />
