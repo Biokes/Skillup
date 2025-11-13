19f1294445333c97e2a79561fd522a168ff98ad4
Biokes <106444765+Biokes@users.noreply.github.com>
2025-11-13 14:58:33 +0100

    update device reposiveness

--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 import { motion } from "framer-motion"
 import { ArrowRight } from "lucide-react"
 
@@ -15,9 +16,9 @@ export default function LandingPage() {
     )
     const Hero = () => (
         <main className='hero'>
-            <motion.div
+            <motion.article
                 animate={{ scale: [1, 1.05, 1], rotate: [0, 1, -1, 0] }}
-                transition={{ duration: 3, ease: "easeInOut", repeat: Infinity, }}
+                transition={{ duration: 3, ease: "easeInOut", repeat: Infinity }}
                 className="text-center mt-20"
             >
                 <h1 className="text-6xl md:text-8xl font-[700] ribeye">
@@ -29,13 +30,68 @@ export default function LandingPage() {
                     </span>
                     <br /> Join the on-chain gaming revolution.
                 </p>
-            </motion.div>
+            </motion.article>
         </main>
+    )
+    const TopGames = () => (
+        <section className='w-full p-2'>
+            <p className='text-gradient ribeye text-[1.5rem] pl-[40px] text-start'>Top Games</p>
+            <article className="flex flex-col md:flex-row px-1 w-full md:h-[300px]">
+                <aside
+                    style={{
+                        backgroundImage: `url(/background1.jpeg)`,
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                    }}
+                    className='rounded-lg overflow-hidden h-[300px] max-w-[500px] w-full flex flex-row items-end'
+                >
+                    <motion.button
+                        animate={{ scale: [1, 1.05, 1], rotate: [0, 1, -1, 0] }}
+                        transition={{ duration: 3, ease: "easeInOut", repeat: Infinity }}
+                        className={'h-[30px] rounded-sm bg-black px-2 ml-5 mb-2 text-glow-cyan ribeye transition-all transform hover:scale-[1.05]'}>
+                        play now
+                    </motion.button>
+                </aside>
+                <section className="flex flex-row sm:flex-col sm:w-[50%] py-2 gap-2 ">
+                    <aside
+                        style={{
+                            backgroundImage: `url(/background2.jpeg)`,
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                        }}
+                        className='rounded-lg overflow-hidden h-[250px] md:h-full  sm:max-w-[300px] w-full flex flex-row items-end'
+                    >
+                        <motion.button
+                            animate={{ scale: [1, 1.05, 1], rotate: [0, 1, -1, 0] }}
+                            transition={{ duration: 3, ease: "easeInOut", repeat: Infinity }}
+                            className={'h-[30px] rounded-sm bg-black px-2 ml-5 mb-2 text-glow-cyan ribeye transition-all transform hover:scale-[1.05]'}>
+                            play now
+                        </motion.button>
+                    </aside>
+                    <aside
+                        style={{
+                            backgroundImage: `url(/background1.jpeg)`,
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                        }}
+                        className='rounded-lg overflow-hidden h-[250px] md:h-full  sm:max-w-[300px] w-full flex flex-row items-end'
+                    >
+                        <motion.button
+                            animate={{ scale: [1, 1.05, 1], rotate: [0, 1, -1, 0] }}
+                            transition={{ duration: 3, ease: "easeInOut", repeat: Infinity }}
+                            className={'h-[30px] rounded-sm bg-black px-2 ml-5 mb-2 text-glow-cyan ribeye transition-all transform hover:scale-[1.05]'}>
+                            play now
+                        </motion.button>
+                    </aside>
+                </section>
+            </article>
+        </section>
     )
     return (
         <main className="w-full">
             <Navbar />
             <Hero />
+            <TopGames />
         </main>
     )
 }
