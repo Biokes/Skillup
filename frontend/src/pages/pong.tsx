1501f19695310934ca110feb6b0a75f72bd5791c
Biokes <106444765+Biokes@users.noreply.github.com>
2025-11-14 19:06:39 +0100

    added inventory to pong arena

--- frontend/src/pages/pong.tsx
+++ frontend/src/pages/pong.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/components/commons/navbar";
 import { Zap } from "lucide-react";
 import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Pong() {
     const games = [
@@ -29,7 +31,27 @@ export default function Pong() {
             action: () => { }
         },
     ]
-    const Livegames = []
+    const powerups = [
+        {
+            name: 'Multiball mayhem',
+            icon: '🎆',
+            description: 'splits the ball for a burst of chaotic offence',
+            owned: 0,
+        },
+        {
+            name: 'Pat Stretch',
+            icon: '💪',
+            description: 'Increase the length of your pat for clutch saves',
+            owned: 0,
+        },
+        {
+            name: 'Guardian Shield',
+            icon: '🛡️',
+            description: 'Summons an energy barrier that block one goal',
+            owned: 0,
+        }
+    ]
+    // const Livegames = []
     const PongHero = () => (
         <div className='pong_hero'>
             <section>
@@ -37,7 +59,7 @@ export default function Pong() {
                     games.map((game, index) => (
                         <motion.article key={index}
                             animate={{ scale: [1, 1.05, 1], rotate: [0, 1, -1, 0] }}
-                            whileHover={{scale: 1.2,  rotate: [0, 2, -2, 0] }}
+                            whileHover={{ scale: 1.2, rotate: [0, 2, -2, 0] }}
                             transition={{ duration: 0.2, ease: "easeInOut", repeat: Infinity }}
                         >
                             {game.icon}
@@ -77,8 +99,26 @@ export default function Pong() {
         </>
     )
     const BoostPack = () => (
-        <section className="sm:h-[200px] flex flex-col sm:flex-col p-1 rounded-lg border-primary border-[2px]">
-            Boost pack
+        <section className="boostpack ">
+            <nav>
+                <h5>Inventory</h5>
+                <Button className={cn("w-[100px] h-[35px] rounded ribeye text-[1rem]")}>refresh</Button>
+            </nav>
+            <div>
+                {
+                    powerups.map((powerUp, index) => (
+                        <motion.article key={index}>
+                            <nav className="flex gap-1 items-center">
+                                <span>{powerUp.icon}</span>
+                                <h5>{powerUp.name}</h5>
+                            </nav>
+                            <p>{powerUp.description}</p>
+                            <h6>Owned: {powerUp.owned}</h6>
+                        </motion.article>
+                    ))
+                }
+            </div>
+            <Button className={cn("w-[160px] h-[35px] rounded ribeye text-[1rem]")}>Open Daily crate</Button>
         </section>
     )
     return (
